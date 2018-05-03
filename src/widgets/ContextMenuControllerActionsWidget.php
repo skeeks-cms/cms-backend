068774eaebc4551033d5ce2a5d3d3f0dd7c8a2f5
Semenov <semenov@skeeks.com>
2018-05-03 09:08:35 +0300

    dev

---
+++ src/widgets/ContextMenuControllerActionsWidget.php
@@ -0,0 +1,114 @@
+<?php
+/**
+ * @author Semenov Alexander <semenov@skeeks.com>
+ * @link https://skeeks.com/
+ * @copyright (c) 2010 SkeekS
+ * @date 16.03.2017
+ */
+
+namespace skeeks\cms\backend\widgets;
+
+use skeeks\cms\backend\BackendAction;
+use skeeks\cms\backend\helpers\BackendUrlHelper;
+use skeeks\yii2\contextmenu\JqueryContextMenuWidget;
+use yii\helpers\Json;
+
+/**
+ * @author Semenov Alexander <semenov@skeeks.com>
+ */
+class ContextMenuControllerActionsWidget extends JqueryContextMenuWidget
+{
+    /**
+     * @var BackendAction[]
+     */
+    public $actions = [];
+    
+    /**
+     * @var bool
+     */
+    public $isOpenNewWindow = false;
+
+    public function run()
+    {
+        $actions = $this->actions;
+
+        $this->view->registerJs(<<<JS
+        
+        jQuery.contextMenu.types.backendAction = function(item, opt, root) {
+            this.addClass('sx-backend-action-wrapper')
+            jQuery('<a>', {
+                'href' : '#',
+                'onclick' : item.onclick,
+            }).append(item.name)
+                .appendTo(this);
+        };
+JS
+        );
+
+        if (!$actions)
+        {
+            return [];
+        }
+        
+        $this->items = [];
+
+        foreach ($actions as $id => $action)
+        {
+            if (!$action->isVisible)
+            {
+                continue;
+            }
+
+            $actionDataJson = Json::encode($this->getActionData($action));
+
+            $options = [];
+            $options['icon'] = $action->icon;
+            $options['type'] = 'backendAction';
+            $options['name'] = "<i class='{$action->icon}'></i> " . $action->name;
+            $options['onclick'] = "new sx.classes.backend.widgets.Action({$actionDataJson}).go(); return false;";
+            
+            $this->items[$action->id] = $options;
+        }
+        
+        return parent::run();
+    }
+    
+    /**
+     * @param AdminAction $action
+     * @return array
+     */
+    public function getActionData($action)
+    {
+        $actionData = array_merge($this->clientOptions, [
+            "url"               => $this->getActionUrl($action),
+
+            //TODO:// is deprecated
+            "isOpenNewWindow"   => $this->isOpenNewWindow,
+            "confirm"           => isset($action->confirm) ? $action->confirm : "",
+            "method"            => isset($action->method) ? $action->method : "",
+            "request"           => isset($action->request) ? $action->request : "",
+        ]);
+
+        return $actionData;
+    }
+    
+    
+    /**
+     * @param $action
+     *
+     * @return array
+     */
+    public function getActionUrl($action)
+    {
+        if (is_array($action->urlData) && $this->isOpenNewWindow)
+        {
+            $action->url = BackendUrlHelper::createByParams($action->urlData)
+                ->enableEmptyLayout()
+                ->enableNoActions()
+                ->params
+            ;
+        };
+
+        return $action->url;
+    }
+}
