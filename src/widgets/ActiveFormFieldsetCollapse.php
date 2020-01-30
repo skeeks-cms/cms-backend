1e26dbf94a1465fba79c1e4625db2482260cddaa
Semenov <semenov@skeeks.com>
2020-01-30 14:25:55 +0300

    delete AdminModelEditorStandartControllerTrait

---
+++ src/widgets/ActiveFormFieldsetCollapse.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * @link https://cms.skeeks.com/
+ * @copyright Copyright (c) 2010 SkeekS
+ * @license https://cms.skeeks.com/license/
+ * @author Semenov Alexander <semenov@skeeks.com>
+ */
+
+namespace skeeks\cms\backend\widgets;
+
+use skeeks\cms\base\widgets\ActiveForm;
+use skeeks\cms\components\Cms;
+use yii\helpers\ArrayHelper;
+use yii\helpers\Html;
+use yii\widgets\ActiveField;
+
+/**
+ * @author Semenov Alexander <semenov@skeeks.com>
+ */
+class ActiveFormFieldsetCollapse extends ActiveForm
+{
+    
+}
