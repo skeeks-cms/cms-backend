b3d7a463a962ad93f91f4b930efa2a1a642b38d6
Semenov <semenov@skeeks.com>
2022-05-06 03:34:28 +0300

    big update

---
+++ src/widgets/ActiveFormAjaxBackend.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * @link https://cms.skeeks.com/
+ * @copyright Copyright (c) 2010 SkeekS
+ * @license https://cms.skeeks.com/license/
+ * @author Semenov Alexander <semenov@skeeks.com>
+ */
+
+namespace skeeks\cms\backend\widgets;
+
+use skeeks\cms\backend\forms\ActiveFieldBackend;
+use skeeks\cms\backend\forms\ActiveFormHasCustomSelectTrait;
+use skeeks\cms\backend\forms\TActiveFormHasButtons;
+use skeeks\cms\backend\forms\TActiveFormHasCustomSelect;
+use skeeks\cms\backend\widgets\assets\BackendFormAsset;
+use skeeks\cms\forms\ActiveFormHasFieldSetsTrait;
+use skeeks\cms\forms\ActiveFormHasPjaxTrait;
+use skeeks\cms\forms\IActiveFormHasFieldSets;
+use skeeks\cms\forms\TActiveFormDynamicReload;
+use skeeks\cms\forms\TActiveFormHasFieldSets;
+use skeeks\cms\forms\TActiveFormHasPjax;
+use skeeks\cms\traits\ActiveFormAjaxSubmitTrait;
+use yii\helpers\Html;
+use yii\widgets\ActiveField;
+use yii\widgets\ActiveForm;
+
+/**
+ * @author Semenov Alexander <semenov@skeeks.com>
+ */
+class ActiveFormAjaxBackend extends ActiveForm implements IActiveFormHasFieldSets
+{
+    use TActiveFormHasFieldSets;
+    use TActiveFormHasButtons;
+    use TActiveFormHasCustomSelect;
+
+    use ActiveFormAjaxSubmitTrait;
+
+    public $enableAjaxValidation = true;
+    public $validateOnChange = false;
+    public $validateOnBlur = false;
+
+    public static $autoIdPrefix = "bf";
+    
+    /**
+     * @var string
+     */
+    //public $fieldClass = ActiveFieldBackend::class;
+    public $fieldClass = ActiveField::class;
+
+    /**
+     * @var bool Подключить стандартные js и css?
+     */
+    public $registerStandartAsset = true;
+
+    /**
+     * @var bool 
+     */
+    public $enableClientValidation = false;
+
+    public function init()
+    {
+        Html::addCssClass($this->options, "sx-backend-form");
+        Html::addCssClass($this->options, "sx-backend-form-inline");
+
+        if ($this->registerStandartAsset) {
+            BackendFormAsset::register($this->view);
+        }
+
+        parent::init();
+
+    }
+
+
+}
