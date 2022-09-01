af94e1e17708f72d87a314e2cc6e6d46e5b4acda
Semenov <semenov@skeeks.com>
2022-09-01 18:15:17 +0300

    Обновление

---
+++ src/views/_model_header.php
@@ -0,0 +1,84 @@
+<?php
+/**
+ * @link https://cms.skeeks.com/
+ * @copyright Copyright (c) 2010 SkeekS
+ * @license https://cms.skeeks.com/license/
+ * @author Semenov Alexander <semenov@skeeks.com>
+ */
+/**
+ * @var $this yii\web\View
+ * @var $model \skeeks\cms\models\CmsContentElement
+ */
+
+$image = null;
+if (isset($model->image)) {
+    $image = $model->image;
+} elseif (isset($model->cmsImage)) {
+    $image = $model->cmsImage;
+}
+$controller = $this->context;
+
+
+?>
+<div class="row" style="margin-bottom: 5px;">
+    <? if ($image) : ?>
+        <div class="col my-auto" style="max-width: 60px">
+            <img style="border: 2px solid #ededed; border-radius: 5px;" src="<?php echo \Yii::$app->imaging->getImagingUrl($image->src,
+                new \skeeks\cms\components\imaging\filters\Thumbnail()); ?>"/>
+        </div>
+    <? endif; ?>
+    <div class="col my-auto">
+        <h1 style="margin-bottom: 0px; line-height: 1.1;">
+            <?php echo $controller->modelShowName; ?>
+        </h1>
+        <div class="sx-small-info" style="font-size: 10px; color: silver;">
+            <span title="ID записи - уникальный код записи в базе данных." data-toggle="tooltip"><i class="fas fa-key"></i> <?php echo $model->id; ?></span>
+            <? if (isset($model->created_at) && $model->created_at) : ?>
+                <span style="margin-left: 5px;" data-toggle="tooltip" title="Запись создана в базе: <?php echo \Yii::$app->formatter->asDatetime($model->created_at); ?>"><i
+                            class="far fa-clock"></i> <?php echo \Yii::$app->formatter->asDate($model->created_at); ?></span>
+            <? endif; ?>
+            <? if (isset($model->created_by) && $model->created_by) : ?>
+                <span style="margin-left: 5px;" data-toggle="tooltip" title="Запись создана пользователем с ID: <?php echo $model->createdBy->id; ?>"><i
+                            class="far fa-user"></i> <?php echo $model->createdBy->shortDisplayName; ?></span>
+            <? endif; ?>
+        </div>
+    </div>
+
+    <?php
+
+    $modelActions = $controller->modelActions;
+    $deleteAction = \yii\helpers\ArrayHelper::getValue($modelActions, "delete");
+
+    if($deleteAction) : ?>
+        <?php
+
+            $actionData = [
+                "url"               => $deleteAction->url,
+
+                //TODO:// is deprecated
+                "isOpenNewWindow"   => true,
+                "confirm"           => isset($deleteAction->confirm) ? $deleteAction->confirm : "",
+                "method"            => isset($deleteAction->method) ? $deleteAction->method : "",
+                "request"           => isset($deleteAction->request) ? $deleteAction->request : "",
+                "size"           => isset($deleteAction->size) ? $deleteAction->size : "",
+            ];
+            $actionData = \yii\helpers\Json::encode($actionData);
+
+            $href = \yii\helpers\Html::a('<i class="fa fa-trash sx-action-icon"></i>', "#", [
+                'onclick' => "new sx.classes.backend.widgets.Action({$actionData}).go(); return false;",
+                'class' => "btn btn-default",
+                'data-toggle' => "tooltip",
+                'title' => "Удалить"
+            ]);
+        ?>
+        <div class="col my-auto" style="text-align: right; max-width: 70px;">
+            <?php echo $href; ?>
+        </div>
+    <?php endif; ?>
+
+
+
+    <!--<div class="col my-auto" style="max-width: 70px; text-align: right;">
+            <a href="<?php /*echo $model->url; */ ?>" data-toggle="tooltip" class="btn btn-default" target="_blank" title="<?php /*echo \Yii::t('skeeks/cms', 'Watch to site (opens new window)'); */ ?>"><i class="fas fa-external-link-alt"></i></a>
+        </div>-->
+</div>
