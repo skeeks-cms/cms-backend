d3dee77d6e4370c7e128e081ec8017a75ac331d7
Semenov <semenov@skeeks.com>
2023-05-24 21:18:20 +0300

    Доработка ЛК

---
+++ src/actions/views/model-update-pjax.php
@@ -0,0 +1,87 @@
+<?php
+/**
+ * @author Semenov Alexander <semenov@skeeks.com>
+ * @link https://skeeks.com/
+ * @copyright (c) 2010 SkeekS
+ * @date 21.12.2017
+ */
+/* @var $this yii\web\View */
+/* @var $controller \skeeks\cms\backend\controllers\BackendModelController */
+/* @var $action \skeeks\cms\backend\actions\BackendModelCreateAction|\skeeks\cms\backend\actions\IHasActiveForm */
+/* @var $model \skeeks\cms\models\CmsLang */
+$controller = $this->context;
+$action = $controller->action;
+
+?>
+
+<? if ($action->beforeContent) : ?>
+    <div class="sx-box sx-p-10 sx-bg-primary" style="margin-bottom: 10px;">
+        <? echo $action->beforeContent; ?>
+    </div>
+<? endif; ?>
+
+<?php $form = $action->beginActiveForm([
+    'enableAjaxValidation'   => false,
+    'enableClientValidation' => false,
+]); ?>
+
+
+<? if ($is_saved && @$is_create) : ?>
+    <?php $this->registerJs(<<<JS
+    sx.Window.openerWidgetTriggerEvent('model-create', {
+        'submitBtn' : '{$submitBtn}'
+    });
+JS
+    ); ?>
+
+<? elseif ($is_saved) : ?>
+    <?php $this->registerJs(<<<JS
+sx.Window.openerWidgetTriggerEvent('model-update', {
+        'submitBtn' : '{$submitBtn}'
+    });
+JS
+    ); ?>
+<? endif; ?>
+
+<? if (@$redirect) : ?>
+    <?php $this->registerJs(<<<JS
+window.location.href = '{$redirect}';
+console.log('window.location.href');
+console.log('{$redirect}');
+JS
+    ); ?>
+<? endif; ?>
+
+
+<?= $form->errorSummary($formModels); ?>
+
+<? echo \Yii::createObject([
+    'class'      => \skeeks\yii2\form\Builder::class,
+    'model'      => $model,
+    'models'     => $formModels,
+    'activeForm' => $form,
+    'fields'     => $action->fields,
+])->render(); ?>
+
+
+<?php $successMessage = ''; ?>
+<?php if(@$is_create) : ?>
+
+<?php else : ?>
+<? if ($successMessageFlash = \Yii::$app->getSession()->getFlash('success')) : ?>
+    <?php $successMessage = $successMessageFlash; ?>
+<? endif; ?>
+<?php endif; ?>
+
+
+<?= $form->buttonsStandart($model, $action->buttons, $successMessage); ?>
+<?= $form->errorSummary($formModels); ?>
+
+<?php $form::end(); ?>
+
+
+<? if ($action->afterContent) : ?>
+    <div class="sx-box sx-p-10 sx-bg-primary" style="margin-bottom: 10px;">
+        <? echo $action->afterContent; ?>
+    </div>
+<? endif; ?>
